8b0f6a92485f7c243f8d00c0bb88c5fc1f00c5c3
Mykhailo-Sizov <sizov.m.i.ko@gmail.com>
2024-12-26 16:33:42 +0200

    wertyj

---
+++ tutor.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// let runy1 = 'abs';
+// let runy = 'qwerty';
+// let ngh = 1234;
+// console.log(runy1, runy, ngh);
+// console.log(123, '123');
+
+
+// const PI = 3.14;
+// const radius = 10;
+// const result = 2 * PI * radius;
+// console.log(result);
+
+
+// let a = '10';
+// let b = '20';
+// console.log(Number(a) + Number(b));
+
+
+
+
+let a = +'2';
+let b = +'3';
+alert(a + b);
